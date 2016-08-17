5d42686ea1b6e9f2cfb369612aba99a5da9cb491
Jerrie Pelser <Jerrie Pelser>
2016-08-17 15:13:38 +0700

    Add OWIN Web API Samples

---
+++ 02-Authentication-HS256/WebApi/WebApi/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var audience = ConfigurationManager.AppSettings["Auth0ClientID"];
+            var secret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["Auth0ClientSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    },
+                });
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }
+}
---
+++ 03-Authorization/WebApi/WebApi/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System.Linq;
+using Microsoft.Owin.Security.OAuth;
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var audience = ConfigurationManager.AppSettings["Auth0ClientID"];
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+                {
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = issuer,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault()?.SecurityKeys?.FirstOrDefault()
+                    },
+                    // Setting the MetadataEndpoint so the middleware can download the RS256 certificate
+                    MetadataEndpoint = $"{issuer.TrimEnd('/')}/wsfed/{audience}/FederationMetadata/2007-06/FederationMetadata.xml"
+                });
+            
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }
+}
