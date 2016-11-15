5a28b562149f0cc506f928ff54745a86563a61a7
jerri <jerri@DESKTOP-2OC92Q0>
2016-11-15 16:20:50 +0700

    Add API Auth samples

---
+++ 04-Authentication-RS256-ApiAuth/WebApi/WebApi/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System.Linq;
+using Microsoft.Owin.Security.OAuth;
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var domain = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var apiIdentifier = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+            var clientId = ConfigurationManager.AppSettings["Auth0ClientId"];
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+                {
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = apiIdentifier,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault()?.SecurityKeys?.FirstOrDefault()
+                    },
+                    // Setting the MetadataEndpoint so the middleware can download the RS256 certificate
+                    MetadataEndpoint = $"{domain.TrimEnd('/')}/wsfed/{clientId}/FederationMetadata/2007-06/FederationMetadata.xml"
+                });
+            
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }
+}
---
+++ 05-Authentication-HS256-ApiAuth/WebApi/WebApi/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Text;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var audience = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+            var secret = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["Auth0ApiSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    },
+                });
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }
+}
