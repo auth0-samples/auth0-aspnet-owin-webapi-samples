f9c4d6b9b735cd933fc8c0240942d190aa2dd215
jerri <jerri@DESKTOP-2OC92Q0>
2016-11-24 11:44:50 +0700

    Sample reorg. Add Scope Authorization with scopes sample

---
+++ 02-Authorization/WebApi/WebApi/Controllers/PingController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class PingController : ApiController
+    {
+        [Route("ping")]
+        [HttpGet]
+        public IHttpActionResult Ping()
+        {
+            return Ok(new
+                {
+                    Message = "All good. You don't need to be authenticated to call this."
+                }
+            );
+        }
+
+        [Authorize]
+        [Route("claims")]
+        [HttpGet]
+        public object Claims()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+
+            return claimsIdentity.Claims.Select(c =>
+            new
+            {
+                Type = c.Type,
+                Value = c.Value
+            });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("ping/secure")]
+        public IHttpActionResult PingSecured()
+        {
+            return Ok(new
+            {
+                Message = "All good. You only get this message if you are authenticated."
+            }
+            );
+        }
+    }
+}
