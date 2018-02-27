acd0557f3ac672847b0b0463f282890857324740
Jerrie Pelser <jerrie@jerriepelser.com>
2018-02-27 13:51:20 +0700

    Add sample of manually loading certificate

---
+++ Samples/rs256-local-certificate/WebApi/README.md
@@ -0,0 +1,13 @@
+﻿# Authentication
+
+This sample demonstartes how you can validate an RS256 token in Auth0 by downloading the certificate for the public key and storing it locally as part of your project. The JWT middleware will then load and read this local certificate and use that to verify the signature for JSON Web Tokens.
+
+## Running the example
+
+In order to run the example you need to just start a server. What we suggest is doing the following:
+
+1. Download the certificate for your Auth0 tenant from `https://YOUR_AUTH0_DOMAIN/cer` and save it as a file named `auth0.cer` in this project folder
+1. Make sure `web.config` contains your credentials. You can find your credentials in the settings section of your Auth0 API.
+1. Hit F5 to start local web development server.
+
+Go to `http://localhost:58105/api/ping` and you'll see the app running :).
---
+++ Samples/rs256-local-certificate/WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Hosting;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var domain = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var apiIdentifier = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+
+            string certificatePath = HostingEnvironment.MapPath("~/auth0.cer");
+            var certificate = new X509Certificate2(certificatePath);
+
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = apiIdentifier,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (a, b, c, d) => new X509SecurityKey(certificate),
+                    }
+                });
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }   
+}
